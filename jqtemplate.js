86df8c063edb803f292c9b682bb346f8dc94202c
Jim Palmer <jimpalmer@gmail.com>
2010-04-30 05:59:35 +0000

    0.2 allowing for non-table block templates with .itr class + removed useless spaces + examples

--- jqtemplate.js
+++ jqtemplate.js
@@ -1,31 +1,32 @@
 /*
  * jQTemplate - fast and light template engine - jQuery plugin
- *  Version 0.1
+ *  Version 0.2
  * Author: Jim Palmer
  * Released under MIT license.
  */
 (function($) {
-	$.fn.jQTemplate = function ( data, dir, template ) {
+	$.fn.jQTemplate = function (data, mod, template) {
 		// clone the template and clobber the target
-		$(this).empty().append( $(template).clone() );
+		$(this).empty().append($(template).clone());
 		// detect iterative or non-iterative root
-		var tbody = $('tbody:first', this).length ? $('tbody:first', this) : this, tbodyTemplate = $(tbody).html(), repHtml = [];
-		// build iterrated pre-dom-insertion
-		if ( typeof data.length != 'undefined' ) {
-			for ( var d=0, de = data.length; d < de; d++)
+		var tbody=$('tbody:first,.itr:first', this).length ? $('tbody:first,.itr:first', this) : this, tbodyTemplate=$(tbody).html(), repHtml=[];
+		// build iterated pre-dom-insertion
+		if (typeof data.length != 'undefined') {
+			for (var d=0, de=data.length; d < de; d++)
 				repHtml.push(tbodyTemplate);
-			// insert iterrative HTML to tbody target all at once
+			// insert iterative HTML to tbody target all at once
 			$(tbody).html(repHtml.join(''));
-		} else // otherwise we're not iterrating
-			data = [data];
-		// apply directives on data with prepend and append support
-		for ( var col in $.extend( {}, data[0], dir) )
+		} else { // otherwise we're not iterating
+			data=[data];
+		}
+		// apply modifiers on data with prepend and append support
+		for (var col in $.extend({}, data[0], mod))
 			$('.' + col, tbody).each(function (i) {
-				var cellVal = typeof data == 'object' && i < data.length && typeof data[i][col] != 'undefined' ? data[i][col] : '';
+				var cellVal=(typeof data == 'object' && i < data.length && typeof data[i][col] != 'undefined') ? data[i][col] : '';
 				$(this).html(
-					( typeof dir['+' + col] == 'function' ? dir['+' + col](i, cellVal) : '' ) +
-					( typeof dir[col] == 'function' ? dir[col](i, cellVal) : cellVal ) +
-					( typeof dir[col + '+'] == 'function' ? dir[col + '+'](i, cellVal) : '' )
+					(typeof mod['+' + col] == 'function' ? mod['+' + col](i, cellVal) : '') +
+					(typeof mod[col] == 'function' ? mod[col](i, cellVal) : cellVal) +
+					(typeof mod[col + '+'] == 'function' ? mod[col + '+'](i, cellVal) : '')
 				);
 			});
 		return this; // chainable
